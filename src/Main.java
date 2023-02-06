82cbbb3d871d545887387450985b5a62e926d424
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-02-05 23:26:28 -0500

    completed project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    double cardBalance = 5000;
+    double interestRate = 0.17;
+    double interestMonth1;
+    double interestMonth2;
+    interestMonth1 = cardBalance * interestRate;
+    interestMonth2 = (interestMonth1 + cardBalance) * interestRate;
+        System.out.println("The interest for the first month is " + interestMonth1 + " and the interest for the second month is " + interestMonth2 );
+
+
+
+
     }
 }
